fb7a8c4f43b381ce996559200ea937211a8510fd
allanfpc <allanfpcosta@gmail.com>
2024-01-10 23:09:32 -0300

    Add new auth Modal for more interactive interface

--- social/src/pages/Login/Login.jsx
+++ social/src/pages/Login/Login.jsx
@@ -1,128 +1,71 @@
-import { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-import Input from "../../components/Input";
-import Button from "../../components/Button";
+import Form from "../../components/Form";
 
 import { useAuthContext } from "../../contexts/AuthContext";
 
-import { loginValidation } from '../../utils/inputValidations';
-import validateForm from "../../utils/formValidation";
+import { loginValidation } from "../../utils/inputValidations";
 
 const initialData = {
-  email: '',
-  password: '',
-}
+	email: "",
+	password: ""
+};
 
 export const Login = () => {
+	const { signIn } = useAuthContext();
+	const [formData, setFormData] = useState(initialData);
+	const [formErrors, setFormErrors] = useState([]);
+	const [error, setError] = useState(null);
 
-  const {signIn} = useAuthContext();
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState(initialData);
-  const [formErrors, setFormErrors] = useState([]);
-  const [error, setError] = useState(null);
-  const loginInputRefs = useRef([]);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = await signIn(formData);
 
-  const err = formErrors.reduce((acc, el) => {
-    acc[el.name] = el;    
-    return acc;
-  }, []);
-
-  useEffect(() => {
-    loginInputRefs.current['email'].focus();    
-  }, []);
-
-  const handleSignIn = (e) => {
-    const validate = validateForm(formData, loginValidation);
-
-    if(validate.errors) {
-      const errors = validate.errors
-      setFormErrors(errors);      
-      
-      const firstErrorRef = loginInputRefs.current[errors[0].name];
-      firstErrorRef.focus();
-
-      e.preventDefault();
-      return false;
-    }
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await signIn(formData);
-
-    if(!data) {
-      return;
-    }
-
-    if(data.error) {
-      setError(data.error);
-    }
-
-    if(data.success) {
-      setFormErrors([]);
-      setFormData(initialData);
-    }
-
-    if(data.token) {
-			navigate('/');
-			return true;
+		if (!data) {
+			return;
 		}
-  }
 
-  const changeText = (e, validation) => {        
-    if(formErrors.length > 0) {      
-      const filteredErrors = formErrors.filter((err) => (
-        err.name !== validation.name
-      ))
-      setFormErrors(filteredErrors);
-    }
-    
-    setFormData({...formData, [validation.name]: err[validation.name] ? e.nativeEvent.data : e.target.value})
-  }
+		if (data.error) {
+			setError(data.error);
+		}
 
-  return (
-    <div>
-      <div className="form-container">
-        <div className="form-title">
-          <h2>Login</h2>
-        </div>
-        <form onSubmit={handleSubmit} noValidate>
-          <div className="form-body">
-            {loginValidation.map((validation, i) => (
-              <Input
-                className={err[validation.name] ? 'error' : ''}
-                key={validation.id}
-                inputRef={(ref) => loginInputRefs.current[validation.name] = ref}
-                label={validation.label}                      
-                name={validation.name}
-                type={validation.type}
-                id={validation.id}                      
-                minlength={validation.validation.minLength?.value}
-                maxlength={validation.validation.maxLength?.value}
-                value={err[validation.name] ? '' : formData[validation.name]}
-                error={err[validation.name]}
-                onChange={(e) => changeText(e, validation)}                    
-              />              
-            ))}              
-            {error && (
-              <div className="error-container">
-                <span>{error}</span>
-              </div>
-            )} 
-          </div>
-          <div className="form-actions">
-            <div className="flex">
-                <Button type="submit" title="Login" className="btn-filled" onClick={(e) => handleSignIn(e)}/>
-            </div>
-            <div className="form-message">
-              <a href="/register">
-                <p className="cursor-pointer regular-14 text-blue-300">Don't have an account? Sign Up</p>
-              </a>
-            </div>
-          </div>
-        </form>               
-      </div>
-    </div>
-  )
-}
+		if (data.success) {
+			setFormErrors([]);
+			setFormData(initialData);
+		}
+
+		if (data.token) {
+			location.assign("/");
+		}
+	};
+
+	return (
+		<div>
+			<div className="form-container">
+				<div className="form-title">
+					<h2>Login</h2>
+				</div>
+				<Form
+					title={"Login"}
+					validator={loginValidation}
+					formData={formData}
+					setFormData={setFormData}
+					formErrors={formErrors}
+					setFormErrors={setFormErrors}
+					error={error}
+					onSubmit={handleSubmit}
+				>
+					<div className="form-actions">
+						<div className="form-message">
+							<a href="/register">
+								<p className="cursor-pointer regular-14 text-blue-300">
+									Don&apos;t have an account? Sign Up
+								</p>
+							</a>
+						</div>
+					</div>
+				</Form>
+			</div>
+		</div>
+	);
+};
--- social/src/pages/Register/Register.jsx
+++ social/src/pages/Register/Register.jsx
@@ -1,137 +1,83 @@
-import Input from "../../components/Input";
-import Button from "../../components/Button";
-import { useEffect, useRef, useState } from "react";
+import Form from "../../components/Form";
+import { useState } from "react";
 import { useAuthContext } from "../../contexts/AuthContext";
 
-import { loginValidation, registerValidation } from '../../utils/inputValidations';
-
-import validateForm from "../../utils/formValidation";
+import {
+	loginValidation,
+	registerValidation
+} from "../../utils/inputValidations";
 
 const initialData = {
-  name: '',
-  nickname: '',
-  email: '',
-  password: '',
-}
+	name: "",
+	nickname: "",
+	email: "",
+	password: ""
+};
 
 const validations = [...registerValidation, ...loginValidation];
 
 export const Register = () => {
+	const { signUp } = useAuthContext();
+	const [registered, setRegistered] = useState(false);
+	const [formErrors, setFormErrors] = useState([]);
+	const [error, setError] = useState(null);
+	const [formData, setFormData] = useState(initialData);
 
-  const {signUp} = useAuthContext();
-  const [registered, setRegistered] = useState(false);
-  const [formData, setFormData] = useState(initialData);
-  const [formErrors, setFormErrors] = useState([]);
-  const [error, setError] = useState(null);
-  const registerInputRefs = useRef([]);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = await signUp(formData);
 
-  const err = formErrors.reduce((acc, el) => {
-    acc[el.name] = el;    
-    return acc;
-  }, []);
+		if (!data) {
+			return;
+		}
 
-  useEffect(() => {
-    registerInputRefs.current['name'].focus();    
-  }, []);
+		if (data.error) {
+			setError(data.error);
+		}
 
-  const handleSignUp = (e) => {
-    const validate = validateForm(formData, validations);
-    
-    if(validate.errors) {
-      const errors = validate.errors
-      setFormErrors(errors);      
-      
-      const firstErrorRef = registerInputRefs.current[errors[0].name];
-      firstErrorRef.focus();
+		if (data.success) {
+			setFormErrors([]);
+			setFormData(initialData);
+			setRegistered(true);
+		}
+	};
 
-      e.preventDefault();
-      return false;
-    }
-  }  
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await signUp(formData);
-
-    if(!data) {
-      return;
-    }
-
-    if(data.error) {
-      setError(data.error);
-    }
-
-    if(data.success) {
-      setFormErrors([])
-      setFormData(initialData);
-      setRegistered(true);
-    }    
-  }
-
-  const changeText = (e, validation) => {
-    if(formErrors.length > 0) {      
-      const filteredErrors = formErrors.filter((err) => (
-        err.name !== validation.name
-      ))
-      setFormErrors(filteredErrors);
-    }
-    
-    setFormData({...formData, [validation.name]: e.target.value})
-  }
-
-  return (
-    <div>
-      <div className="form-container">
-        {registered ? (
-          <div>
-            <h2>Successfully registered! </h2>
-            <a href="/login">Login</a>
-          </div>
-        ) : (
-          <>
-            <div className="form-title">
-              <h2>Register</h2>
-            </div>            
-            <div className="register">
-              <form onSubmit={handleSubmit} noValidate>
-                <div className="form-body">
-                  {validations.map((validation, i) => (
-                    <Input
-                      className={err[validation.name] ? 'error' : ''}
-                      key={validation.id}
-                      inputRef={(ref) => registerInputRefs.current[validation.name] = ref}
-                      label={validation.label}                      
-                      name={validation.name}
-                      type={validation.type}
-                      id={validation.id}                      
-                      minlength={validation.validation.minLength?.value}
-                      maxlength={validation.validation.maxLength?.value}
-                      value={err[validation.name] ? '' : formData[validation.name]}
-                      error={err[validation.name]}
-                      onChange={(e) => changeText(e, validation)}                  
-                    />
-                  ))}                      
-                  {error && (
-                    <div className="error-container">
-                      <span>{error}</span>
-                    </div>
-                  )}
-                </div>                  
-                <div className="form-actions">
-                  <div className="flex">
-                    <Button type="submit" title="Register" className="btn-filled" onClick={(e) => handleSignUp(e)}/>
-                  </div>
-                  <div className="form-message">
-                    <a href="/login">
-                      <p className="cursor-pointer regular-14 text-blue-300">Already have an account? Sign in</p>
-                    </a>
-                  </div>
-                </div>
-              </form>                
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  )
-}
+	return (
+		<div>
+			<div className="form-container">
+				{registered ? (
+					<div>
+						<h2>Successfully registered! </h2>
+						<a href="/login">Login</a>
+					</div>
+				) : (
+					<>
+						<div className="form-title">
+							<h2>Register</h2>
+						</div>
+						<div className="register">
+							<Form
+								title={"Register"}
+								validator={validations}
+								formData={formData}
+								setFormData={setFormData}
+								formErrors={formErrors}
+								setFormErrors={setFormErrors}
+								error={error}
+								onSubmit={handleSubmit}
+							>
+								<div className="form-message">
+									<a href="/login">
+										<p className="cursor-pointer regular-14 text-blue-300">
+											Already have an account? Sign in
+										</p>
+									</a>
+								</div>
+							</Form>
+						</div>
+					</>
+				)}
+			</div>
+		</div>
+	);
+};
