c6c8424dac4b866e2c26ab505e934072250a037b
allanfpc <allanfpcosta@gmail.com>
2024-01-11 23:27:50 -0300

    Improve error handling for target more error types and gracefully crashes

---
+++ social/src/pages/Error/403.jsx
@@ -0,0 +1,16 @@
+export const Page403 = () => {
+	return (
+		<div className="flex-center">
+			<div className="error-container">
+				<div className="">
+					<div className="code">
+						<span>403</span>
+					</div>
+				</div>
+				<div className="message">
+					<span>Forbidden</span>
+				</div>
+			</div>
+		</div>
+	);
+};
